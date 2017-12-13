ba7ff06fd3f5058f4273cc157398ea858cd2c720
asjop <abhay@justoneplanet.co.in>
2017-12-13 11:41:25 +0530

    Circular image with border and border with space between border and image. + with brief note

--- app/src/main/java/com/vivek/android2/circularimage/MainActivity.java
+++ app/src/main/java/com/vivek/android2/circularimage/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Link of library
         // https://github.com/hdodenhof/CircleImageView
 
         // Limitations:
